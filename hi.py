b0ce414a821817bd53f7a268ba8562f2358f417c
Oğuz Batuhan Çözeli <oguzbatuhanczl@gmail.com>
2024-04-23 15:11:26 +0300

    yes no confling

--- hi.py
+++ hi.py
@@ -1,1 +1,25 @@
-print("Hello' World.")
+name = "Oguz"
+print('My name is {}'.format(name))
+
+dizi = ("Antalya", "Ankara", "Adana")
+print(dizi[2])
+
+dizi1 = ("Antalya" "Ankara" "Adana")
+print(dizi1[2:5])
+print(len(dizi1))
+
+number : int = 11
+name : str = "Batuhan"
+
+print("I'm "+name+" and nambur "+str(number))
+
+print(">>>>>>>>>>>")
+
+name = "Oğuz"
+surName = "Batuhan"
+age = "20"
+işlem = 200 / 700
+
+print(f"I'm {name} {surName} years old {age}.")
+print("I'm {} {} years old {}".format(name, surName, age))
+print("{x:3.2}".format(x=işlem))
