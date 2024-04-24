c57b661e384ea575f874b0d816226bc7b542d1ed
Oğuz Batuhan Çözeli <oguzbatuhanczl@gmail.com>
2024-04-24 10:28:47 +0300

    age calculation

---
+++ ageCalculation.py
@@ -0,0 +1,5 @@
+print("Calculate your age. >>")
+year = input("You enter the year: ")
+birthyear = input("Enter your year of birth: ")
+conclusion = int(year) - int(birthyear)
+print(">> "+str(conclusion)+" You are years old.")
