2c982c032056bfd964037891ddda932033c4a30b
Oğuz Batuhan Çözeli <oguzbatuhanczl@gmail.com>
2024-04-27 13:50:55 +0300

    str-method

--- hi.py
+++ hi.py
@@ -23,3 +23,24 @@ işlem = 200 / 700
 print(f"I'm {name} {surName} years old {age}.")
 print("I'm {} {} years old {}".format(name, surName, age))
 print("{x:3.2}".format(x=işlem))
+
+print(">>>>>>>>>")
+
+message = "   Hi python. hi Oğuz. hi Sude."
+print(message)
+print(message.upper())
+print(message.title())
+print(message.lower())
+print(message.capitalize())
+print(message.strip())
+dizi = (message.split("."))
+print(dizi[0])
+print(dizi[2] + dizi[1])
+print(" ".join(dizi))
+print(message.find("Sude"))
+print(message.startswith(" "))
+print(message.endswith("."))
+message2 = message.replace("ğ","g").replace(" ","/").replace(",",".")
+print(message2)
+
+ 
